b21ad5419ea65e3c7b558e967edf4ed6c1350dfc
agalewa91 <135745526+agalewa91@users.noreply.github.com>
2023-08-16 15:46:24 +0200

    dodanie zadania

--- 5-walidacja-formularza-zapisu-do-newslettera/js/script.js
+++ 5-walidacja-formularza-zapisu-do-newslettera/js/script.js
@@ -4,48 +4,48 @@ let newsletterForm = document.getElementById("newsletter-form");
 console.log(newsletterForm);
 let allAgreeChx = document.getElementById("all-agree");
 
-// const validate = (event) => {
-//   // event.preventDefault();
-//   console.log("vaildate");
-//   let txtName = document.getElementById("name");
-//   let txtEmail = document.getElementById("email");
-//   let agree1 = document.getElementById("agree-1");
-//   let errors = document.getElementById("errors");
-
-//   errors.innerHTML = "";
-
-//   if (txtName.value.trim() === "") {
-//     let liError = document.createElement("li");
-//     liError.innerText = "Wpisz imię i nazwisko!";
-//     errors.appendChild(liError);
-//   }
-
-//   if (txtEmail.value.trim() === "") {
-//     let liError = document.createElement("li");
-//     liError.innerText = "Wpisz adres e-mail!";
-//     errors.appendChild(liError);
-//   }
-
-//   if (!txtEmail.value.includes("@")) {
-//     let liError = document.createElement("li");
-//     liError.innerText = "Adres e-mail musi zawierać @!";
-//     errors.appendChild(liError);
-//   }
-
-//   if (!agree1.checked) {
-//     let liError = document.createElement("li");
-//     liError.innerText = "Nie wyraziłeś zgody 1!";
-//     errors.appendChild(liError);
-//   }
-
-//   if (errors.children.length > 0) {
-//     event.preventDefault();
-//   }
-// };
-
 const validate = (event) => {
-  console.log("validate");
+  // event.preventDefault();
+  console.log("vaildate");
+  let txtName = document.getElementById("name");
+  let txtEmail = document.getElementById("email");
+  let agree1 = document.getElementById("agree-1");
+  let errors = document.getElementById("errors");
+
+  errors.innerHTML = "";
+
+  if (txtName.value.trim() === "") {
+    let liError = document.createElement("li");
+    liError.innerText = "Wpisz imię i nazwisko!";
+    errors.appendChild(liError);
+  }
+
+  if (txtEmail.value.trim() === "") {
+    let liError = document.createElement("li");
+    liError.innerText = "Wpisz adres e-mail!";
+    errors.appendChild(liError);
+  }
+
+  if (!txtEmail.value.includes("@")) {
+    let liError = document.createElement("li");
+    liError.innerText = "Adres e-mail musi zawierać @!";
+    errors.appendChild(liError);
+  }
+
+  if (!agree1.checked) {
+    let liError = document.createElement("li");
+    liError.innerText = "Nie wyraziłeś zgody 1!";
+    errors.appendChild(liError);
+  }
+
+  if (errors.children.length > 0) {
+    event.preventDefault();
+  }
 };
+
+// const validate = (event) => {
+//   console.log("validate");
+// };
 const allAgree = (event) => {
   let agree1 = document.getElementById("agree-1");
   let agree2 = document.getElementById("agree-2");
