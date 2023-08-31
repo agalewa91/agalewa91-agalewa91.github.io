97d546371e6b799807152e4b19bc6c70ef0287f0
agalewa91 <135745526+agalewa91@users.noreply.github.com>
2023-08-31 16:44:30 +0200

    zadanie ajax

---
+++ 3-pobierz-dane-po-kliknieciu-przycisku-jquery/js/script.js
@@ -0,0 +1,56 @@
+console.log("Warsztat");
+
+$(document).ready(function () {
+  $("#get-data").click(function () {
+    // Wariant 1
+    //     $.get("https://akademia108.pl/api/ajax/get-post.php")
+    //       .done(function (data) {
+    //         let pId = $("<p></p>").text("Post ID: ${data.id}");
+    //         let pUserId = $("<p></p>").text("Post ID: ${data.userId}");
+    //         let pTitle = $("<p></p>").text("Post ID: ${data.title}");
+    //         let pBody = $("<p></p>").text("Post ID: ${data.body}");
+    //         let hr = $("<hr/>");
+
+    //         let jqBody = $("body");
+
+    //         jqBody.append(pId);
+    //         jqBody.append(pUserId);
+    //         jqBody.append(pTitle);
+    //         jqBody.append(pBody);
+    //         jqBody.append(hr);
+
+    //         console.log(pId);
+    //       })
+
+    //       .fail(function (error) {
+    //         console.error(error);
+    //       });
+    //   });
+    // });
+
+    // Wariant 2
+
+    $.getJSON("https://akademia108.pl/api/ajax/get-post.php")
+      .done(function (data) {
+        let pId = $("<p></p>").text("Post ID: ${data.id}");
+        let pUserId = $("<p></p>").text("Post ID: ${data.userId}");
+        let pTitle = $("<p></p>").text("Post ID: ${data.title}");
+        let pBody = $("<p></p>").text("Post ID: ${data.body}");
+        let hr = $("<hr/>");
+
+        let jqBody = $("body");
+
+        jqBody.append(pId);
+        jqBody.append(pUserId);
+        jqBody.append(pTitle);
+        jqBody.append(pBody);
+        jqBody.append(hr);
+
+        console.log(pId);
+      })
+
+      .fail(function (error) {
+        console.error(error);
+      });
+  });
+});
